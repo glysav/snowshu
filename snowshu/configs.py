f11788294492efa4e73d6838f481855845781bbc
glysav <73849510+glysav@users.noreply.github.com>
2024-04-12 17:16:18 +0300

    Update configs.py

--- snowshu/configs.py
+++ snowshu/configs.py
@@ -23,7 +23,7 @@ DEFAULT_TEMPORARY_DATABASE = 'SNOWSHU'
 
 
 def _is_in_docker() -> bool:
-    # return False
+    return True
     # running horizontal,
     # this should work for Unix AND Windows
     # https://stackoverflow.com/questions/36765138/bind-to-docker-socket-on-windows
